ee7ecfba2d6b909e640b0a93bdd1313fe799c98b
z4ch <35627271+zachzwei@users.noreply.github.com>
2024-08-29 10:57:22 +0800

    Create bell20minter.sh
    important:
    change the destination address and the .json file to the bel20 you are trying to mint
    if you do not do that, my address will get your mints

---
+++ bell20minter.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo inscribing moar NOOK! 
+echo please be patient anon...
+node . mint BMbuF3ZLgcyWmMgMNFMqdLeHVd8JsDuBDo nook.json 50
+sleep 30
+node . mint BMbuF3ZLgcyWmMgMNFMqdLeHVd8JsDuBDo nook.json 50
+sleep 30
+node . mint BMbuF3ZLgcyWmMgMNFMqdLeHVd8JsDuBDo nook.json 50
+echo finished minting 150x nook.json
+echo check tx confirmation before printing moar!
